611d3fe3b523bc9c6bea92a8e1ac5330d5d0a006
Deni <denividan@gmail.com>
2023-05-10 14:48:03 +0200

    deploy v1.4

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use("/api", route)
 
 const port = 3030
