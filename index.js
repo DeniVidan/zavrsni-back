a5f702c8510ee2cf026f9219eca089982405a39a
Deni <denividan@gmail.com>
2023-05-11 13:24:05 +0200

    deploy v1.5

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://bookrestaurant.netlify.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+  });
 
 app.use("/api", route)
 
